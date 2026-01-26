1d1b89a236b3ee05c13124088a7a4096f7f0fd26
Murilo Dias de Oliveira <murilodiasdeoliveira2022@gmail.com>
2026-01-26 09:49:34 -0300

    adaptação da home para mobile

--- script.js
+++ script.js
@@ -155,3 +155,22 @@ setInterval(() => {
       botao.disabled = false;
     }
   });
+
+
+
+
+  const hamburger = document.querySelector(".hamburger");
+const navegacao = document.querySelector(".navegacao");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navegacao.classList.toggle("active");
+});
+
+// Opcional: Fechar o menu ao clicar em um link
+document.querySelectorAll(".navegacao a").forEach(link => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navegacao.classList.remove("active");
+  });
+});
