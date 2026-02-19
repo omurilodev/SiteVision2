e7be39626c4b27e62197e302d0e2411c285c9cbc
Murilo Dias de Oliveira <murilodiasdeoliveira2022@gmail.com>
2026-02-19 16:16:26 -0300

    mobile adaptado

--- planos.js
+++ planos.js
@@ -45,3 +45,19 @@ if (track) {
 
   animate();
 }
+
+  const hamburger = document.querySelector(".hamburger");
+const navegacao = document.querySelector(".navegacao");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navegacao.classList.toggle("active");
+});
+
+// Opcional: Fechar o menu ao clicar em um link
+document.querySelectorAll(".navegacao a").forEach(link => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navegacao.classList.remove("active");
+  });
+});
